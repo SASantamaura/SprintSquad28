c147078ebe914353337d05f986a639c67742d0c2
SHERNET\santamau <supseby@gmail.com>
2018-10-30 13:14:02 -0400

    First Commit - Initial Project
    Basic Player Movement

---
+++ Box Stealth/Assets/scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float turnSpeed;
+    bool detectable;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (Input.GetKeyUp("space"))
+        {
+            Debug.Log("Standing up");
+            transform.position = new Vector3(transform.position.x, 1.25f, transform.position.z);
+            detectable = true;
+        }
+
+        if (!Input.GetKey("space"))
+        {
+            //float z = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+            float y = Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed;
+
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            transform.Rotate(0, y, 0);
+        }
+        else
+        {
+            detectable = false; 
+            transform.position = new Vector3(transform.position.x, 1f, transform.position.z);
+        }
+
+
+	}
+}
