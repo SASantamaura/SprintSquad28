a81bddd04a3703b7a69594e570f2d8ddd4685636
SHERNET\santamau <supseby@gmail.com>
2018-11-01 16:03:40 -0400

    Added Sounds - Footsteps and looping drone

---
+++ Box Stealth/Assets/Scripts/PlayerSteps.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSteps : MonoBehaviour {
+
+    AudioSource feet;
+    public AudioClip step1;
+    public AudioClip step2;
+    public AudioClip step3;
+
+	// Use this for initialization
+	void Start ()
+    {
+        feet = GetComponent<AudioSource>();
+
+	}
+	
+	// Update is called once per frame
+	public void OnFootStep ()
+    {
+        int currentSound = Random.Range(1, 4);
+
+        switch(currentSound)
+        {
+            case 1:
+                feet.PlayOneShot(step1);
+                break;
+            case 2:
+                feet.PlayOneShot(step2);
+                break;
+            case 3:
+                feet.PlayOneShot(step3);
+                break;
+            default:
+                feet.PlayOneShot(step3);
+                break;
+        }
+    }
+}
