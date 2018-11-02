97e0457299bb3644b6081dea5a0591999f3aaa53
SHERNET\santamau <supseby@gmail.com>
2018-11-02 10:47:39 -0400

    APK BUILD

--- Box Stealth/Assets/Scripts/GyroTestScript.cs
+++ Box Stealth/Assets/Scripts/GyroTestScript.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.Animations;
 using UnityEngine.UI;
 
 public class GyroTestScript : MonoBehaviour {
